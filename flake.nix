4971069d2699c22de35ab3940a44a734f8d6e59c
Fran <fkrausear@gmail.com>
2024-08-30 22:30:24 -0300

    setup: use flake.nix
    setup: refine flake.nix

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys =
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, rust-overlay, ... }@inputs:
+    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
+
+      devShells = forEachSystem (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs { inherit system overlays; };
+        in {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [{
+              # https://devenv.sh/reference/options/
+              packages = with pkgs; [
+                rust-bin.stable.latest.default
+                openssl
+                pkg-config
+                fish
+                sea-orm-cli
+              ];
+
+              enterShell = "";
+
+              processes.migrate = "sea-orm-cli migrate up";
+              services.postgres = {
+                enable = true;
+                package = pkgs.postgresql_16;
+                initialDatabases = [{ name = "postgres"; }];
+              };
+            }];
+          };
+        });
+    };
+}
